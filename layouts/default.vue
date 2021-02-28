11f1f11401081475ef2bd2a8f2fe0e951d438642
sonu yadav <sonuyadav94220@gmail.com>
2021-02-28 11:24:48 +0530

    booston template

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <front-navbar/>
+    <Nuxt />
+    <myfooter />
+  </div>
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+ <div class="container-fluid">
+   <div class="row">
+     <div class="col-md-12 p-0">
+       <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+         <div class="carousel-inner">
+           <div class="carousel-item active">
+             <banner />
+           </div>
+           <div  class="carousel-item">
+             <banner />
+           </div>
+         </div>
+         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+           <span class="visually-hidden">Previous</span>
+         </button>
+         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+           <span class="carousel-control-next-icon" aria-hidden="true"></span>
+           <span class="visually-hidden">Next</span>
+         </button>
+       </div>
+     </div>
+   </div>
+   <marketing/>
+   <stress-free-move/>
+   <long-local-distance/>
+   <yelp/>
+   <socal-media-logo/>
+   <phone-email-location/>
+ </div>
+</template>
+
+<script>
+export default {
+  layout:"default",
+
+}
+</script>
